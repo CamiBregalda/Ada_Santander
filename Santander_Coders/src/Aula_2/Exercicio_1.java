c55aa46dc90d565629aa9151231df6e3fb24fce9
Camila Bregalda <108535010+CamiBregalda@users.noreply.github.com>
2023-07-14 20:44:17 -0300

    Update

--- Santander_Coders/src/Aula_2/Exercicio_1.java
+++ Santander_Coders/src/Aula_2/Exercicio_1.java
@@ -2,7 +2,7 @@ package Aula_2;
 
 import java.util.Scanner;
 
-public class Exercicio_2 {
+public class Exercicio_1 {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
