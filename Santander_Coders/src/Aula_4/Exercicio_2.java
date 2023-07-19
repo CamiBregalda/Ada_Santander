91076a92eb0c65d11c226f32448470aa5db66b2e
Camila Bregalda <108535010+CamiBregalda@users.noreply.github.com>
2023-07-19 20:08:53 -0300

    Update Exercicio_2.java

--- Santander_Coders/src/Aula_4/Exercicio_2.java
+++ Santander_Coders/src/Aula_4/Exercicio_2.java
@@ -1,15 +1,24 @@
-/*João é um empresário do ramo de construção civil e decidiu adotar novos critérios para dar reajuste salarial para seus funcionários. Agora, 
-o reajuste vai ser dado seguindo as regras abaixo.
+/*João é um empresário do ramo de construção civil e decidiu adotar novos critérios para dar reajuste salarial para seus funcionários. 
+Agora, o reajuste vai ser dado seguindo as regras abaixo.
 
 Tempo de serviço:
-de 1 até 5 anos => 1%
-de 5 até 10 anos => 1.5%
-10 ou mais anos => 2% valor da inflação (IPCA)
+menos de 1 ano => somente inflação
+de 1 até 5 anos (menos que 5 anos) => 1% + inflação
+de 5 até 10 anos (menos que 10 anos) => 1.5% + inflação
+10 ou mais anos => 2% + inflação
 O valor total percentual para o reajuste vai ser a soma do percentual de tempo de serviço mais o percentual da inflação.
 
 Para ajudar João a calcular o valor do reajuste salarial de seus funcionários, crie uma função que recebe um array como entrada; onde, a posição 0 
-do array é o tempo de serviço, a posição 1 é o valor da inflação e a posição 3 é o salário do funcionário. Sua função deve calcular o novo salário 
-e retorna-lo. Considere arredondamento de duas casas decimais para o salário retornado.*/
+do array é o tempo de serviço, a posição 1 é o valor da inflação e a posição 2 é o salário do funcionário. Sua função deve calcular o novo salário 
+e retorna-lo. Considere arredondamento de duas casas decimais para o salário retornado.
+
+Ex:
+Entrada: [1, 5.0, 2000.00]
+Saída: 2120.00
+
+Ex:
+Entrada: [11,4.5,2500.00]
+Saída: 2662.50*/
 
 package Aula_4;
 
